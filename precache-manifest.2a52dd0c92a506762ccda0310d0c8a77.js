e46dc725871e2b4447a6cd8e6400d2e2d65c5454
Zhydovlenko <you@example.com>
2020-07-28 11:43:39 +0300

    Updates

---
+++ precache-manifest.2a52dd0c92a506762ccda0310d0c8a77.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73a06d9528ac70f3f39bed5ba2c287f2",
+    "url": "/goit-react-hw-08-phonebook/index.html"
+  },
+  {
+    "revision": "91beb5ed8734fc3f7c62",
+    "url": "/goit-react-hw-08-phonebook/static/css/2.a123c8b4.chunk.css"
+  },
+  {
+    "revision": "70e3a6160f42f8cbf8d3",
+    "url": "/goit-react-hw-08-phonebook/static/css/3.86b3705f.chunk.css"
+  },
+  {
+    "revision": "980aa208bf32ae871b39",
+    "url": "/goit-react-hw-08-phonebook/static/css/4.f34240e3.chunk.css"
+  },
+  {
+    "revision": "e1d94e79ef3656b2e38c",
+    "url": "/goit-react-hw-08-phonebook/static/css/5.f34240e3.chunk.css"
+  },
+  {
+    "revision": "6696a9451b010a9f2e76",
+    "url": "/goit-react-hw-08-phonebook/static/css/6.f34240e3.chunk.css"
+  },
+  {
+    "revision": "ae6b0d4ed3ce888ec41d",
+    "url": "/goit-react-hw-08-phonebook/static/css/main.96f88c0c.chunk.css"
+  },
+  {
+    "revision": "91beb5ed8734fc3f7c62",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.da157056.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.da157056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70e3a6160f42f8cbf8d3",
+    "url": "/goit-react-hw-08-phonebook/static/js/3.f5758ffb.chunk.js"
+  },
+  {
+    "revision": "980aa208bf32ae871b39",
+    "url": "/goit-react-hw-08-phonebook/static/js/4.bb2d81ae.chunk.js"
+  },
+  {
+    "revision": "e1d94e79ef3656b2e38c",
+    "url": "/goit-react-hw-08-phonebook/static/js/5.1f59c9f9.chunk.js"
+  },
+  {
+    "revision": "6696a9451b010a9f2e76",
+    "url": "/goit-react-hw-08-phonebook/static/js/6.be09a7a6.chunk.js"
+  },
+  {
+    "revision": "4d6951b72c640e994f5c",
+    "url": "/goit-react-hw-08-phonebook/static/js/7.0b8a27f5.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/goit-react-hw-08-phonebook/static/js/7.0b8a27f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae6b0d4ed3ce888ec41d",
+    "url": "/goit-react-hw-08-phonebook/static/js/main.5b4e2b0b.chunk.js"
+  },
+  {
+    "revision": "4a2b96d30eb6c2bce02a",
+    "url": "/goit-react-hw-08-phonebook/static/js/runtime-main.cdff22fd.js"
+  },
+  {
+    "revision": "37c53933612002d12b9fadd3236d1eeb",
+    "url": "/goit-react-hw-08-phonebook/static/media/contact-book.37c53933.png"
+  }
+]);
